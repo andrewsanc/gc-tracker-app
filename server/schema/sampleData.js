adedf6643e3d1cf807e0339409dbdbbc7fe247a5
Andrew Sanchez <druu831@gmail.com>
2023-03-10 11:31:31 -0800

    added sample data

---
+++ server/schema/sampleData.js
@@ -0,0 +1,104 @@
+// Golfcourse
+const golfCourseData = [
+  {
+    name: "Augusta National Golf Club",
+    location: {
+      city: "Augusta",
+      state: "Georgia",
+    },
+    holes: 18,
+    par: 72,
+  },
+  {
+    name: "Pebble Beach Golf Links",
+    location: {
+      city: "Pebble Beach",
+      state: "California",
+    },
+    holes: 18,
+    par: 72,
+  },
+];
+
+// Players
+const playersData = [
+  {
+    player: "John Smith",
+    course: ObjectId("615e5d0ef0c82b6858125e2c"),
+    date: new Date("2022-05-01"),
+    scores: [
+      {
+        holeNumber: 1,
+        score: 4,
+      },
+      {
+        holeNumber: 2,
+        score: 5,
+      },
+      {
+        holeNumber: 3,
+        score: 3,
+      },
+      {
+        holeNumber: 4,
+        score: 4,
+      },
+      {
+        holeNumber: 5,
+        score: 4,
+      },
+      {
+        holeNumber: 6,
+        score: 3,
+      },
+      {
+        holeNumber: 7,
+        score: 4,
+      },
+      {
+        holeNumber: 8,
+        score: 5,
+      },
+      {
+        holeNumber: 9,
+        score: 4,
+      },
+      {
+        holeNumber: 10,
+        score: 4,
+      },
+      {
+        holeNumber: 11,
+        score: 3,
+      },
+      {
+        holeNumber: 12,
+        score: 5,
+      },
+      {
+        holeNumber: 13,
+        score: 4,
+      },
+      {
+        holeNumber: 14,
+        score: 3,
+      },
+      {
+        holeNumber: 15,
+        score: 4,
+      },
+      {
+        holeNumber: 16,
+        score: 4,
+      },
+      {
+        holeNumber: 17,
+        score: 3,
+      },
+      {
+        holeNumber: 18,
+        score: 4,
+      },
+    ],
+  },
+];
